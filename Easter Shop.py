e99150f463b7e473562a99585781661df776b074
EmanuilManev <62472027+EmanuilManev@users.noreply.github.com>
2020-03-22 00:13:46 +0200

    Add files via upload

---
+++ Easter Shop.py
@@ -0,0 +1,22 @@
+available = int(input())
+order = str("")
+qtty = int()
+sold = 0
+while order != "Close":
+    order = input()
+    if order == "Fill":
+        qtty = int(input())
+        available += qtty
+    elif order == "Buy":
+        qtty = int(input())
+        if (available - qtty) > 0:
+            available -= qtty
+            sold += qtty
+        else:
+            print("Not enough eggs in store!")
+            print("You can buy only " + str(available) + ".")
+            break
+else:
+    if order == "Close":
+        print("Store is closed!")
+        print(str(sold) + " eggs sold.")
