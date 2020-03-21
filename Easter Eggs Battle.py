8d753a5369648a125451d4cada588e27418732d9
EmanuilManev <62472027+EmanuilManev@users.noreply.github.com>
2020-03-21 19:17:47 +0200

    Add files via upload

--- Easter Eggs Battle.py
+++ Easter Eggs Battle.py
@@ -2,21 +2,16 @@ player1 = int(input())
 player2 = int(input())
 
 battle = str("")
-while player1 > 0 and player2 >0 and battle != "End of battle":
+while player1 > 0 and player2 > 0 and battle != "End of battle":
     battle = input()
     if battle == "one":
         player2 = player2 - 1
     elif battle == "two":
         player1 = player1 - 1
-    elif battle == "End of battle":
-        print("Player one has " + str(player1) + " eggs left.")
-        print("Player two has " + str(player2) + " eggs left.")
 
 if player1 > player2 and battle != "End of battle":
-    print("Player two is out of eggs.")
-    print("Player one has " + str(player1) + " eggs left.")
+   print("Player two is out of eggs. Player one has " + str(player1) + " eggs left.")
 elif player2 > player1 and battle != "End of battle":
-    print("Player one is out of eggs.")
-    print("Player two has " + str(player2) + " eggs left.")
-
-
+   print("Player one is out of eggs. Player two has " + str(player2) + " eggs left.")
+else:
+    print("Player one has " + str(player1) + " eggs left. Player two has " + str(player2) + " eggs left.")
