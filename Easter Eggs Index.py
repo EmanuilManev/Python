4ca7ef5f8cd77f2a901e487e6ff25563bfad9bff
EmanuilManev <62472027+EmanuilManev@users.noreply.github.com>
2020-03-22 11:04:41 +0200

    Add files via upload

--- Easter Eggs Index.py
+++ Easter Eggs Index.py
@@ -1,17 +1,14 @@
 eggs = int(input())
 index = 1
-red = 0
-orange = 0
-blue = 0
-green = 0
-max_eggs = [red, orange, blue, green]
+
+max_eggs = [0, 0, 0, 0]
 colours = ["red", "orange", "blue", "green"]
 for index in range(eggs):
     colour = input()
     max_eggs[colours.index(colour)] += 1
 
-print("Red eggs: " + str(red))
-print("Orange eggs: " + str(orange))
-print("Blue eggs: " + str(blue))
-print("Green eggs: " + str(green))
+print("Red eggs: " + str(max_eggs[1]))
+print("Orange eggs: " + str(max_eggs[2]))
+print("Blue eggs: " + str(max_eggs[3]))
+print("Green eggs: " + str(max_eggs[4]))
 print("Max eggs: " + str(max(max_eggs)) + " -> " + colours[max_eggs.index(max(max_eggs))])
